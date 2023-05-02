52baa71060596f3a235ecc71de637501cfa1701f
Matthew A Johnson <matjoh@microsoft.com>
2023-05-02 18:01:00 +0100

    Adding memoization (#4)
    This commit adds straightforward memoization during the rules pass.
    
    Signed-off-by: Matthew A Johnson <matjoh@microsoft.com>

--- src/lookup.h
+++ src/lookup.h
@@ -6,12 +6,7 @@ namespace rego
 {
   using namespace trieste;
 
-  Node singleton_value(const Nodes& defs);
-
   Node search(const Node& lookup);
-
-  bool any_refs(NodeDef& n);
-
   bool can_replace(const NodeRange& n);
   bool exists(const NodeRange& n);
 }
