647c8f86222b3d7971c6f9b96a226adf60dc631a
Matthew A Johnson <matjoh@microsoft.com>
2023-09-09 23:36:03 +0100

    Updating build script to do everything in out dir
    Signed-off-by: Matthew A Johnson <matjoh@microsoft.com>

--- wrappers/rust/regorust/build.rs
+++ wrappers/rust/regorust/build.rs
@@ -3,46 +3,51 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
     Command::new("git")
         .args(&["clone", "https://github.com/matajoh/rego-cpp.git"])
+        .current_dir(&out_path)
         .status()
         .expect("failed to execute process");
 
+    let regocpp_path = out_path.join("rego-cpp");
+
     Command::new("git")
         .args(&["checkout", "rust-api"])
-        .current_dir("rego-cpp")
+        .current_dir(&regocpp_path)
         .status()
         .expect("failed to execute process");
 
     Command::new("cmake")
         .args(&["-S", ".", "-B", "build", "--preset", "release-rust"])
-        .current_dir("rego-cpp")
+        .current_dir(&regocpp_path)
         .status()
         .expect("failed to execute process");
+
+    let regocpp_build_path = regocpp_path.join("build");
 
     Command::new("ninja")
         .args(&["install"])
-        .current_dir("rego-cpp/build")
+        .current_dir(&regocpp_build_path)
         .status()
         .expect("failed to execute process");
+
+    let regocpp_build_rust_path = regocpp_build_path.join("rust");
+    let rego_path = out_path.join("rego");
 
     Command::new("cp")
-        .args(&["-r", "rego-cpp/build/rust", "rego"])
+        .args(&[
+            "-r",
+            regocpp_build_rust_path.to_str().unwrap(),
+            rego_path.to_str().unwrap(),
+        ])
         .status()
         .expect("failed to execute process");
 
-    Command::new("rm")
-        .args(&["-rf", "rego-cpp"])
-        .status()
-        .expect("failed to execute process");
+    let libdir_path = rego_path.join("lib");
 
-    let libdir_path = PathBuf::from("rego/lib")
-        .canonicalize()
-        .expect("cannot canonicalize path");
-
-    let headers_path = PathBuf::from("rego/include/rego/rego_c.h")
-        .canonicalize()
-        .expect("cannot canonicalize path");
+    let headers_path = rego_path.join("include").join("rego").join("rego_c.h");
     let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
 
     println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
@@ -58,7 +63,6 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
