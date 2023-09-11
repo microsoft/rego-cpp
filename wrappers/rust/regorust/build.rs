1e385b1c82533d474da201a57345bcc5074b7436
Matthew Johnson <matjoh@microsoft.com>
2023-09-11 01:59:41 +0100

    Fixing Windows crate build
    Signed-off-by: Matthew Johnson <matjoh@microsoft.com>

--- wrappers/rust/regorust/build.rs
+++ wrappers/rust/regorust/build.rs
@@ -8,7 +8,7 @@ fn main() {
     let regocpp_path = out_path.join("rego-cpp");
     let num_procs = std::thread::available_parallelism().unwrap_or(NonZeroUsize::new(1).unwrap());
 
-    if !out_path.exists() {
+    if !regocpp_path.exists() {
         Command::new("git")
             .args(&["clone", "https://github.com/matajoh/rego-cpp.git"])
             .current_dir(&out_path)
@@ -28,8 +28,29 @@ fn main() {
         .status()
         .expect("failed to execute process");
 
+    let regocpp_config = match std::env::var("PROFILE").as_ref() {
+        Ok(build_type) => {
+            if build_type == "debug" {
+                "RelWithDebInfo"
+            } else {
+                "Release"
+            }
+        }
+        Err(_) => "Release",
+    };
+
     Command::new("cmake")
-        .args(&["-S", ".", "-B", "build", "--preset", "release-rust"])
+        .args(&[
+            "-S",
+            ".",
+            "-B",
+            "build",
+            "-DCMAKE_BUILD_TYPE={}"
+                .replace("{}", regocpp_config)
+                .as_str(),
+            "-DCMAKE_INSTALL_PREFIX=rust",
+            "-DREGOCPP_COPY_EXAMPLES=ON",
+        ])
         .current_dir(&regocpp_path)
         .status()
         .expect("failed to execute process");
@@ -40,6 +61,8 @@ fn main() {
         .args(&[
             "--build",
             ".",
+            "--config",
+            regocpp_config,
             "--parallel",
             &num_procs.to_string(),
             "--target",
@@ -49,7 +72,7 @@ fn main() {
         .status()
         .expect("failed to execute process");
 
-    let rego_path = regocpp_build_path.join("rust");
+    let rego_path = regocpp_path.join("rust");
     let libdir_path = rego_path.join("lib");
     let headers_path = rego_path.join("include").join("rego").join("rego_c.h");
     let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
@@ -58,7 +81,11 @@ fn main() {
     println!("cargo:rustc-link-lib=static=re2");
     println!("cargo:rustc-link-lib=static=snmallocshim-static");
     println!("cargo:rustc-link-lib=static=rego");
-    println!("cargo:rustc-link-lib=stdc++");
+    if cfg!(windows) {
+        println!("cargo:rustc-link-arg=mincore.lib");
+    } else {
+        println!("cargo:rustc-link-lib=stdc++");
+    }
     println!("cargo:rerun-if-changed={}", headers_path.to_str().unwrap());
 
     let bindings = bindgen::Builder::default()
