61bb221494c583d9fc5f7e0778d1ce4f89d15c2f
Matthew A Johnson <matjoh@microsoft.com>
2025-10-17 00:00:00 +0100

    Updating the dotnet docs and example deps (#192)
    Signed-off-by: Matthew A Johnson <matjoh@microsoft.com>

---
+++ examples/dotnet/MyPolicy/Program.cs
@@ -0,0 +1,23 @@
+﻿
+RoomAccess roomAccess = new RoomAccess("""
+{
+    "users": {
+        "alice": {
+            "clearance": ["alpha", "gamma"]
+        },
+        "bob": {
+            "clearance": ["beta", "gamma"]
+        },
+        "charlie": {
+            "security_staff": true
+        }
+    },
+    "rooms": ["alpha", "beta", "gamma"]
+}
+""");
+
+roomAccess.Check("alice", "alpha");
+roomAccess.Check("bob", "alpha");
+roomAccess.Check("charlie", "gamma");
+roomAccess.Check("doug", "alpha");
+roomAccess.Check("charlie", "epsilon");
