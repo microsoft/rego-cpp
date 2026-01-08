d8f141390bd3c1d317602f8b571d00907728eeed
Matthew A Johnson <matjoh@microsoft.com>
2026-01-08 14:01:11 +0000

    Version 1.2.0 (#205)
    Minor version fixing some bugs.
    
    **Bug Fixes**
    - When passing a value to `sprintf` using `%v` as the format specifier, the
      interpreter would JSON-escape the result in all cases, even if the argument
      already a valid JSON string. This could result in double-escaping strings in
      some contexts, which was an unintended behavior and a discrepancy between with
      the reference implementation. The interpreter will now pass JSON strings
      unchanged to `sprintf` when used in this way.
    - Two rules in `rego_to_bundle` were being shadowed. This did not affect the
      functionality of the compiler but had resulted in an incorrect error message
      being generated and in another case a rule which would never be matched.
    
    Signed-off-by: Matthew A Johnson <matjoh@microsoft.com>

--- wrappers/python/docs/source/conf.py
+++ wrappers/python/docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'regopy'
 copyright = 'Microsoft'
 author = 'Microsoft'
-release = '1.1.0'
+release = '1.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
